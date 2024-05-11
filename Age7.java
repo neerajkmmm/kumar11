3934b8d79c4a068e1eb4b5558b13bff2f4ae4d0d
neerajkmmm <neerajparmar180@gmail.com>
2024-05-11 12:36:55 -0700

    first commit

---
+++ Age7.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class Age7{
+public static void main(String[]args){
+Scanner ob=new Scanner(System.in);
+System.out.print("Enter 1st age: ");
+           int age1=ob.nextInt();
+           System.out.print("Enter 2nd age:");
+           int age2=ob.nextInt();
+           System.out.print("Enter 3rd age:");
+           int age3=ob.nextInt();
+		   System.out.print("Enter fourth age:");
+		   int age4=ob.nextInt();
+		   System.out.print("Enter 5th age:");
+		   int age5=ob.nextInt();
+		   System.out.print("Enter 6th age:");
+		   int age6=ob.nextInt();
+		   if(age1>age2){
+			   if(age1>age3){
+				   if(age1>age4){
+					   if(age1>age5){
+						   if(age1>age6){
+		   System.out.println("1st age is greater:");}}}}}
+		   if(age2>age1){
+			   if(age2>age3){
+				   if(age2>age4){
+				   if(age2>age5){
+					   if(age2>age6){
+		   System.out.println("2nd age is greater:");}}}}}
+		   if(age3>age1){
+			   if(age3>age2){
+				   if(age3>age4){
+					   if(age3>age5){
+						   if(age3>age6){
+		   System.out.println("3rd age is greater:");}}}}}
+		   if(age4>age1){
+			   if(age4>age2){
+				   if(age4>age3){
+					   if(age4>age5){
+						   if(age4>age6){
+		   System.out.println("4rth age is greater:");}}}}}
+		   if(age5>age1){
+			   if(age5>age2){
+				   if(age5>age3){
+					   if(age5>age4){
+						   if(age4>age6){
+		   System.out.println("5th age is greater:");}}}}}
+
+           if(age6>age1){
+			   if(age6>age2){
+				   if(age6>age3){
+					   if(age6>age4){
+						   if(age6>age5){
+		   System.out.println("6th age is greater:");}}}}}
+}
+}
+
